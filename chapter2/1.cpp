761a39dc15a5c8ba7a4209ba7d580375b6aa242f
liuhe <liuhe@oceanum.cn>
2023-11-07 19:11:42 +0800

    补齐第2章

---
+++ chapter2/1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+int main() {
+  cout << "please enter your name: ";
+  string name;
+  cin >> name;
+  const string greeting = "Hello, " + name + "!";
+  int pad = 0;
+  const int rows = pad * 2 + 3;
+  const string::size_type cols = greeting.size() + pad * 2 + 2;
+  for (int r = 0; r != rows; r++) {
+    string::size_type c = 0;
+    while(c != cols) {
+      if (r == pad + 1 && c == pad + 1) {
+        cout << greeting;
+        c += greeting.size();
+      } else {
+        if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
+          cout << "*";
+        } else {
+          cout << " ";
+        }
+        ++c;
+      }
+    }
+    cout << endl;
+  }
+  return 0;
+}
+*/
+int main() {
+  cout << "please enter your name: ";
+  string name;
+  cin >> name;
+  const string greeting = "Hello, " + name + "!";
+  const string::size_type cols = greeting.size() + 2;
+  for(int r = 0; r != 3; r++) {
+    if (r == 0 || r == 2) {
+      cout << string(cols, '*') << endl;
+    }
+    else{
+      cout << '*' + greeting + '*' << endl;
+    }
+  }
+}
