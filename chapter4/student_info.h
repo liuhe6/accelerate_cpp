e3048237c83381b546777444f364cc124e45462f
liuhe <liuhe@oceanum.cn>
2023-11-21 02:45:42 +0800

    add part of chapter 4

---
+++ chapter4/student_info.h
@@ -0,0 +1,18 @@
+#ifndef GUARD_Student_info
+#define GUARD_Student_info
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+struct Student_info {
+    string name;
+    double midterm, final;
+    vector<double> homework;
+};
+
+bool compare(const Student_info&, const Student_info&);
+istream& read(istream&, Student_info&);
+istream& read_hw(istream&, vector<double>&);
+#endif // !GUARD_Student_info
